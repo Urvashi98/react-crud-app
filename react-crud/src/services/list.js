d450849b3e2d83fad2f284df89885163612f3696
Urvashi98 <urvgajjar98@gmail.com>
2022-03-17 16:18:45 +0530

    completed api calls required in this app

--- react-crud/src/services/list.js
+++ react-crud/src/services/list.js
@@ -1,27 +1,27 @@
 import axios from "axios";
 
-// export function getBooksList() {
-//   let response;
-//   try {
-//     const res = axios.get("http://localhost:3333/books");
-//     response = res.data;
-//   } catch (err) {
-//     response = err;
-//   }
-//   return response;
-// }
 
 export function getBooksList() {
   const res = axios.get("http://localhost:3333/books");
   return res;
 }
 
-export async function setBookToList(book) {
-  let response;
-  try {
-    response = await axios.post("http://localhost:3333/books", book);
+export  function setBookToList(book) {
+  let response= axios.post("http://localhost:3333/books", book);
     return response;
-  } catch (err) {
-    return err;
-  }
+}
+
+export function deleteBook(id) {
+  const res = axios.delete(`http://localhost:3333/books/${id}`);
+  return res;
+}
+
+export function getBookById(id) {
+  const res = axios.get(`http://localhost:3333/books/${id}`);
+  return res;
+}
+
+export function updateBook(id, book) {
+  const res = axios.put(`http://localhost:3333/books/${id}`, book);
+  return res;
 }
